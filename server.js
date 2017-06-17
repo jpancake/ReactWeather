03417cfd108a70f2630d88fcd325794d9571e118
jpancake <jrazon_18@yahoo.com>
2017-06-16 23:25:12 -0700

    api key

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3050
 
+app.use((req, res, next) => {
+	if (req.headers['x-forwarded-proto'] === 'https') {
+		res.redirect(`http://${req.hostname + req.url}`)
+	} else {
+		next()
+	}
+})
+
 app.use(express.static(path.resolve(__dirname, 'public')))
 
 app.set('views', path.resolve(__dirname, 'public/'))
